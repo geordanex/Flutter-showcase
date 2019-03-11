eec8c351d4979a9031cb9c3af9e30683d9293c82
Geordan Sánchez <geordan.sanchez@entelgy.com>
2019-03-11 13:02:32 -0500

    Implementacion del InputRadio

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,50 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-  bool _value1 = false;
-  bool _value2 = false;
+  int _value1 = 0;
+  int _value2 = 0;
 
-  void _value1Changed(bool value) => setState(() => _value1 = value);
+  void _setValue1(int value) => setState(() => _value1 = value);
 
-  void _value2Changed(bool value) => setState(() => _value2 = value);
+  void _setValue2(int value) => setState(() => _value2 = value);
+
+  Widget makeRadios() {
+    List<Widget> list = [];
+
+    for (int i = 0; i < 3; i++) {
+      list.add(new Radio(
+        value: i,
+        groupValue: _value1,
+        onChanged: _setValue1,
+      ));
+    }
+
+    Column column = new Column(
+      children: list,
+    );
+    return column;
+  }
+
+    Widget makeRadioTiles() {
+    List<Widget> list = [];
+
+    for (int i = 0; i < 3; i++) {
+      list.add(new RadioListTile(
+        value: i,
+        groupValue: _value2,
+        onChanged: _setValue2,
+        activeColor: Colors.green,
+        controlAffinity: ListTileControlAffinity.trailing,
+        title: new Text('Item: ${i}'),
+        subtitle: new Text('Subtitle')
+      ));
+    }
+
+    Column column = new Column(
+      children: list,
+    );
+    return column;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,19 +64,8 @@ class _State extends State<MyApp> {
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Checkbox(
-                value: _value1,
-                onChanged: _value1Changed,
-              ),
-              new CheckboxListTile(
-                value: _value2,
-                onChanged: _value2Changed,
-                title: new Text('Hello World'),
-                controlAffinity: ListTileControlAffinity.leading,
-                subtitle: new Text('Subtitle'),
-                secondary: new Icon(Icons.archive),
-                activeColor: Colors.red,
-              )
+              makeRadios(),
+              makeRadioTiles()
             ],
           ),
         ),
