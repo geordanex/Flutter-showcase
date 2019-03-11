e6dba291abb5cb8a761835c1fc22607049c505a3
Geordan Sánchez <geordan.sanchez@entelgy.com>
2019-03-11 11:40:39 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  @override
+  _State createState() => new _State();
+}
+
+class _State extends State<MyApp> {
+  String _value = 'Hello World';
+
+  void _onPressed(String value) {
+    setState(() {
+      _value = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('My App'),
+      ),
+      body: new Container(
+        padding: new EdgeInsets.all(32.0),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              new Text(_value),
+              new RaisedButton(
+                onPressed: () => _onPressed('Hello Geordan'),
+                child: new Text('Click me'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
