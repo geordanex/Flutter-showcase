2ba5ff095aa992b7c853c1714b4b79af99f9812d
Geordan Sánchez <geordan.sanchez@entelgy.com>
2019-03-13 10:26:35 -0500

    Implementacion del Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,33 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-
-  String _value = '';
-  void _onClick() => setState(() => _value = new DateTime.now().toString());
-
+  
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('My App'),
       ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _onClick,
-        backgroundColor: Colors.red,
-        mini: false,
-        child: new Icon(Icons.timer),
+      drawer: new Drawer(
+        child: new Container(
+          padding: new EdgeInsets.all(32.0),
+          child: new Column(
+            children: <Widget>[
+              new Text("Hello Drawer"),
+              new RaisedButton(
+                onPressed: () => Navigator.pop(context),
+                child: new Text('Close')
+              )
+            ],
+          ),
+        ),
       ),
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
             children: <Widget>[
-              new Text(_value)
+              new Text('Hello')
             ],
           ),
         ),
