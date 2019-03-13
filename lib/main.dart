9571515fe3f1023bb7deba3f5c94d3ed984c71c8
Geordan Sánchez <geordan.sanchez@entelgy.com>
2019-03-13 10:32:16 -0500

    Implementacion Footer Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -8,34 +8,34 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
-  
+  String _value = '';
+  void _onClick(String value) => setState(() => _value = value);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('My App'),
       ),
-      drawer: new Drawer(
-        child: new Container(
-          padding: new EdgeInsets.all(32.0),
-          child: new Column(
-            children: <Widget>[
-              new Text("Hello Drawer"),
-              new RaisedButton(
-                onPressed: () => Navigator.pop(context),
-                child: new Text('Close')
-              )
-            ],
-          ),
+      persistentFooterButtons: <Widget>[
+        new IconButton(
+          icon: new Icon(Icons.timer),
+          onPressed: () => _onClick('Button 1'),
         ),
-      ),
+        new IconButton(
+          icon: new Icon(Icons.toc),
+          onPressed: () => _onClick('Button 2'),
+        ),
+        new IconButton(
+          icon: new Icon(Icons.card_giftcard),
+          onPressed: () => _onClick('Button 3'),
+        ),
+      ],
       body: new Container(
         padding: new EdgeInsets.all(32.0),
         child: new Center(
           child: new Column(
-            children: <Widget>[
-              new Text('Hello')
-            ],
+            children: <Widget>[new Text(_value)],
           ),
         ),
       ),
